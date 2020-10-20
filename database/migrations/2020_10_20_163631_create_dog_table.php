1a617a62119418910d1677fd0685b4f3c2938964
Juan <juanjosbrenes@gmail.com>
2020-10-20 21:58:13 +0200

    Comiezo del proyecto

---
+++ database/migrations/2020_10_20_163631_create_dog_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('photo');
+            $table->integer('weight');
+            $table->string('size');
+            $table->string('race');
+            $table->string('sex');
+            $table->date('birth');
+            $table->string('color');
+            $table->date('chip_date');
+            $table->date('location_chip');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dogs');
+    }
+}
